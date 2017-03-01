ff5a9a32d8cef7dd7909b0b54e803d2ebd5c7d70
Peter Williams <peter@newton.cx>
2017-03-01 13:13:41 -0500

    Attempt to fix #7: busted pkg-config file
    This commit has some diagnostics that will be removed after I check that the
    fix works.

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,8 +2,12 @@
 
 mkdir build && cd build
 
-cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_LIBDIR=$PREFIX/lib
+cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_LIBDIR=lib
 cmake --build . --config Release --target install
 
-cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_LIBDIR=$PREFIX/lib
+cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_LIBDIR=lib
 cmake --build . --config Release --target install
+
+echo TEMPORARY DIAGNOSTIC
+cat $PREFIX/lib/pkgconfig/libssh2.pc
+echo END DIAGNOSTIC
