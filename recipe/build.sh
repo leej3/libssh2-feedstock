ad6f7287a9dfc7182973ecbe6696d8551f85d0c0
Uwe L. Korn <uwe.korn@quantco.com>
2020-07-09 14:36:26 +0200

    CFEP-18: Remove static libraries from main build

--- recipe/build.sh
+++ recipe/build.sh
@@ -16,18 +16,6 @@ mkdir build && cd build
 
 cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
       -D CMAKE_PREFIX_PATH=$PREFIX \
-      -D BUILD_SHARED_LIBS=OFF \
-      -D CRYPTO_BACKEND=OpenSSL \
-      -D CMAKE_INSTALL_LIBDIR=lib \
-      -D ENABLE_ZLIB_COMPRESSION=ON \
-      $SRC_DIR
-
-make -j${CPU_COUNT}
-# ctest  # fails on the docker image
-make install
-
-cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
-      -D CMAKE_PREFIX_PATH=$PREFIX \
       -D BUILD_SHARED_LIBS=ON \
       -D CRYPTO_BACKEND=OpenSSL \
       -D CMAKE_INSTALL_LIBDIR=lib \
